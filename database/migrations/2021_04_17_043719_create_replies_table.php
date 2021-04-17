b50c77d94172479fd995d97af55230cd4e068457
Vipin32 <36256756+Vipin32@users.noreply.github.com>
2021-04-17 10:39:54 +0530

    Creating Models, Migrations, Controllers, Factories

---
+++ database/migrations/2021_04_17_043719_create_replies_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRepliesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('replies', function (Blueprint $table) {
+            $table->id();
+            $table->text('body');
+
+            $table->integer('question_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+            
+            $table->foreign('question_id')->references('id')->on('questions')->onDelete('cascade'); //if question is deleted :Delete all replies related to that question
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('replies');
+    }
+}
