8fd6abd1f52881fbb9cf54a74c178a0bfc3f3ca7
Vipin32 <36256756+Vipin32@users.noreply.github.com>
2021-04-17 11:20:29 +0530

    Reply Model Relationship

--- app/Models/Reply.php
+++ app/Models/Reply.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Reply extends Model
 {
     use HasFactory;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function question()
+    {
+        return $this->belongsTo(Question::class);
+    }
 }
